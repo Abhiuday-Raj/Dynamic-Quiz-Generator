98732b929dfbd94cc07c71f96ab467ce8e3ddaf3
Abhiuday Raj <158555735+Abhiuday-Raj@users.noreply.github.com>
2024-05-03 09:37:00 +0530

    Add files via upload

---
+++ javascript/question1.js
@@ -0,0 +1,76 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    difficulty: "easy",
+    numb: 1,
+    question: "Current Affairs 1",
+    answer: "Option 1",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "easy",
+    numb: 2,
+    question: "Current Affairs 2",
+    answer: "Cascading Style Sheet",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "easy",
+    numb: 3,
+    question: "Current Affairs 3",
+    answer: "Option 3",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "easy",
+    numb: 4,
+    question: "Current Affairs 4",
+    answer: "Option 2",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "intermediate",
+    numb: 5,
+    question: "Current Affairs 5",
+    answer: "Option 1",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  {
+    difficulty: "intermediate",
+    numb: 6,
+    question: "Current Affairs 6",
+    answer: "Option 1",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "mintermediate",
+    numb: 7,
+    question: "Current Affairs 7",
+    answer: "Option 1",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "hard",
+    numb: 8,
+    question: "Current Affairs 8",
+    answer: "Option 1",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "hard",
+    numb: 9,
+    question: "Current Affairs 9",
+    answer: "Option 1",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+  {
+    difficulty: "hard",
+    numb: 10,
+    question: "Current Affairs10",
+    answer: "Option 1",
+    options: ["Option 1", "option 2", "option 3", "option 4"],
+  },
+];
